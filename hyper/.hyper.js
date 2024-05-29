630bcdb30f5edb8777e108d5864b9afd0430ef9f
Anne Brown <anne@browndomain.com>
2024-05-29 12:38:19 -0400

    added hyper config search

--- hyper/.hyper.js
+++ hyper/.hyper.js
@@ -145,7 +145,7 @@ module.exports = {
     //   `hyperpower`
     //   `@company/project`
     //   `project#1.0.1`
-    plugins: ['hyperborder', 'hyper-rename-tab', 'hyper-save-window-state', 'hyper-opacity'],
+    plugins: ['hyperborder', 'hyper-rename-tab', 'hyper-save-windowstate', 'hyper-opacity', 'hyper-search'],
     // in development, you can create a directory under
     // `~/.hyper_plugins/local/` and include it here
     // to load it and avoid it being `npm install`ed
