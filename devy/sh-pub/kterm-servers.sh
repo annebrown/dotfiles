7aa27b1b1dcad48915fb3a3615c7ec3ef8d6c81c
Anne Brown <anne@browndomain.com>
2025-05-02 06:05:18 -0400

    Terminal Layouts

--- devy/sh-pub/kterm-servers.sh
+++ devy/sh-pub/kterm-servers.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-konsole  --separate --geometry 610x650+3850+0 --display :0.0 --layout /usr/share/konsole/1x2-terminals.json &
+konsole  --separate --geometry 610x650+3850+0 --display :0.0 --layout ~/.dotfiles/konsole/servers-layout.json &
